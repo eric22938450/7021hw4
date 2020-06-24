b7cc4b6c3f21d3848475f9f8756cc0977da9ccda
jubao <tqy_4896@hotmail.com>
2020-06-25 02:25:25 +0800

    Initial Commit

---
+++ 7021hw4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  7021hw4
+//
+//  Created by User16 on 2020/6/25.
+//  Copyright © 2020 ntou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
